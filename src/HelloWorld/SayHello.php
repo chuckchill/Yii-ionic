6229649aa38f9c0419aad50150cda8adab7e78e4
Albert Qiu <26038180@qq.com>
2016-03-11 22:21:44 +0800

    commit

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,15 @@
+<?php
+namespace HelloWorld;
+/**
+ * Created by PhpStorm.
+ * User: albert
+ * Date: 2016/3/11
+ * Time: 21:43
+ */
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
